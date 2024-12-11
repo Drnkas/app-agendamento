f81434e264604f2d9c58906ecd9bb160f6609ab2
isabela_dranka <isabela.dranka@gadocerto.com.br>
2024-12-10 22:48:26 -0300

    feature(rotas): incio da configuração de rotas

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,19 @@
 import 'package:app_agendamento/core/flavor/flavor.dart';
+import 'package:app_agendamento/core/route/app_routes.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void bootstrap(FlavorConfig config){
+  runApp(App());
+}
 
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: router,
+    );
+  }
 }
---
+++ lib/core/route/app_routes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+          path: '/',
+          builder: (context, state) => Container(color: Colors.pink,)
+      ),
+      GoRoute(
+          path: '/auth',
+          builder: (context, state) => Container(color: Colors.yellow,)
+      )
+    ]
+);
